782f8b91ebbf030e54fdea72112fdc39373c90e3
Lasse Tyrihjell <lassetyr@gmail.com>
2017-03-06 11:18:46 +0100

    ZonedDateTimeAdapter now also supports incoming milliseconds

--- src/main/java/org/rutebanken/siri20/adapter/ZonedDateTimeAdapter.java
+++ src/main/java/org/rutebanken/siri20/adapter/ZonedDateTimeAdapter.java
@@ -1,9 +1,6 @@
 package org.rutebanken.siri20.adapter;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
@@ -14,18 +11,39 @@ public class ZonedDateTimeAdapter {
      *
      * If Zone is not provided, local system default is used
      *
-     * @param dateTime
+     * @param dateTime May be either ISO-formatted string, or timestamp in milliseconds
      * @return
      */
     public static ZonedDateTime parse(String dateTime) {
         ZonedDateTime parsed;
-        try {
-            parsed = ZonedDateTime.parse(dateTime);
-        } catch (DateTimeParseException e) {
-            LocalDateTime parse1 = LocalDateTime.parse(dateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-            parsed =ZonedDateTime.ofLocal(parse1, ZoneId.systemDefault(), ZoneOffset.ofHours(0));
+        if (dateTime!= null && isNumeric(dateTime)) {
+            parsed = parse(Long.valueOf(dateTime));
+        } else {
+            try {
+                parsed = ZonedDateTime.parse(dateTime);
+            } catch (DateTimeParseException e) {
+                LocalDateTime parse1 = LocalDateTime.parse(dateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+                parsed = ZonedDateTime.ofLocal(parse1, ZoneId.systemDefault(), ZoneOffset.ofHours(0));
 
+            }
         }
         return parsed.withZoneSameInstant(ZoneId.systemDefault());
+    }
+
+    private static boolean isNumeric(String str)
+    {
+        return str.matches("\\d+");  //match a number with optional '-' and decimal.
+    }
+
+    /**
+     * Parses dateTime to ZonedDateTime with optional zone
+     *
+     * If Zone is not provided, local system default is used
+     *
+     * @param dateTime
+     * @return
+     */
+    private static ZonedDateTime parse(long dateTime) {
+        return ZonedDateTime.ofInstant(Instant.ofEpochSecond(dateTime), ZoneId.systemDefault());
     }
 }
